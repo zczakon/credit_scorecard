da14300d56cefe83e6a90be8b9f0e0dc119fbad9
zczakon <56118290+zczakon@users.noreply.github.com>
2021-01-06 15:44:50 +0100

    group division in hosmer lemeshow

--- calibration_assessment.py
+++ calibration_assessment.py
@@ -1,8 +1,34 @@
+import pandas as pd
+import numpy as np
 
+class CalibrationMetrics(data):
+    def __init__(self):
+        self.data = data
 
-class CalibrationMetrics:
-    def group_to_centiles(self):
-        pass
+    def divide_to_groups(self,predicted_proba , n=10):
+        """
+
+        :param predicted_proba:
+        :param n:
+        :return: list od 2d arrays of default flag and predicted probability of default
+        """
+        observations=self.data['DEFAULT_FLAG']
+        observations = observations.to_numpy()
+        num_of_obs = len(observations)
+        obs_and_preds = np.concatenate(observations, predicted_proba ,axis=1)
+
+        group_size = num_of_obs//n
+        groups=[]
+
+        for i in range(n):
+            if i <= n-2:
+                groups.append(obs_and_preds[i*group_size:(i+1)*group_size,:])
+            else:
+                groups.append(obs_and_preds[(n-1)*group_size:,:])
+        return groups
+
+    def sort_groups(self, predicted_proba):
+
 
     #calibration accuracy
     def hosmer_lemeshow(self):
