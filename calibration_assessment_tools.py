71432e2310d8faa6abe66fe5c2e0d760ed0d09c7
zczakon <56118290+zczakon@users.noreply.github.com>
2021-01-23 14:18:45 +0100

    implied model true_negative fixed

---
+++ calibration_assessment_tools.py
@@ -0,0 +1,146 @@
+import numpy as np
+from scipy.stats import norm
+
+
+class Utils:
+    def __init__(self, data):
+        self.data = data
+
+    def divide_to_groups(self, predicted_probability, n=10):
+        """
+        :param predicted_probability: 1d array of PDs (nums between 0&1, 1=certain default)
+        :param n: desired number of groups, defaults to 10
+        :return: list od 2d arrays of PDs and default flagst
+        """
+        observations = self.data[:, np.shape(self.data)[1] - 1]
+        num_of_obs = np.shape(observations)[0]
+
+        obs_and_predictions = np.column_stack((observations, predicted_probability))
+        group_size = num_of_obs // n
+
+        groups = []
+        for i in range(n):
+            if i <= n - 2:
+                groups.append(obs_and_predictions[i * group_size:(i + 1) * group_size, :])
+            else:
+                groups.append(obs_and_predictions[(n - 1) * group_size:, :])
+        return groups
+
+    @staticmethod
+    def avg_group_proba(group):
+        """
+        :param group: 2d array of PDs and default flags
+        :return: average PD in a group
+        """
+        pds = group[:, 1]  # sum of probabilities of defaults
+        return np.sum(pds) / len(pds)
+
+    def sorted_groups(self, predicted_probability, n=10):
+        """
+        :param predicted_probability: 1d array of predicted default probabilities (1=certain default)
+        :param n: number of groups
+        :return: n groups of pairs in an array [realized default flag, PD](<-somehow in this order) sorted by avg groups PD in descending order
+        """
+        groups = self.divide_to_groups(predicted_probability, n)
+        avg_probs = [self.avg_group_proba(group) for group in groups]
+
+        groups = np.column_stack((groups, avg_probs))
+
+        sorted_groups = groups[groups[:, 1].argsort()]
+        sorted_groups = np.flip(sorted_groups, axis=0)
+
+        return sorted_groups[:, 0]
+
+
+class CalibrationMetrics(Utils):
+    def __init__(self, data):
+        super().__init__(data)
+        self.data = data
+
+    # calibration accuracy
+    def hosmer_lemeshow(self, groups):
+        """
+        :groups: optional, list of 2d arrays of PDs and default flags
+        :return: list of hosmer lemeshow statistic values for given groups
+        """
+        hs = []
+        for group in groups:
+            total_number = np.shape(group)[0]
+            number_of_defaults = np.sum(group[:, 0])
+            realized_default_rate = number_of_defaults / total_number
+            predicted_default_rate = self.avg_group_proba(group)
+            hs_stat = np.divide((realized_default_rate - predicted_default_rate) ** 2,
+                                predicted_default_rate * (1 - predicted_default_rate)) * total_number
+            hs.append(hs_stat)
+        return hs
+
+    def brier_score(self, groups):
+        total_borrowers = 0
+        bs_group = []
+        for group in groups:
+            borrowers_in_group = np.shape(group)[0]
+            total_borrowers += borrowers_in_group
+            number_of_defaults = np.sum(group[:, 0])
+            realized_default_rate = number_of_defaults / borrowers_in_group
+            predicted_default_rate = self.avg_group_proba(group)
+            summed = realized_default_rate * (1 - realized_default_rate) + (
+                    predicted_default_rate - realized_default_rate) ** 2
+
+            bs_group.append(borrowers_in_group * summed)
+        bs = (1 / total_borrowers) * np.sum(bs_group)
+        return bs
+
+    def brier_skill_score(self, groups, total_defaults, num_of_obs=1161):
+        pd_observed = total_defaults / num_of_obs
+        return 1 - np.divide(self.brier_score(groups), pd_observed * (1 - pd_observed))
+
+    def normal_approximation_bounds(self, group, q):
+        borrowers_in_group = np.shape(group)[0]
+        g_avg = self.avg_group_proba(group)
+        upper_bound = norm.ppf((q + 1) / 2) * np.sqrt(np.divide((g_avg * (1 - g_avg)), borrowers_in_group))
+        lower_bound = g_avg - upper_bound
+        return lower_bound, upper_bound
+
+    def normal_approximation_test(self, group):
+        lower_bound_99, upper_bound_99 = self.normal_approximation_bounds(group, 0.99)
+        lower_bound_95, upper_bound_95 = self.normal_approximation_bounds(group, 0.95)
+        g_avg = self.avg_group_proba(group)
+        if g_avg >= upper_bound_99:
+            rating = 'Green'
+        elif g_avg <= upper_bound_95:
+            rating = 'Red'
+        else:
+            rating = 'Yellow'
+        return rating
+
+
+from logistic_regression import predict_proba, x_test, y_test, total_test_defaults
+#from probit_regression import predict_proba, x_test, y_test, total_test_defaults
+
+y_test = np.reshape(y_test, (1161, 1))
+data = np.concatenate((x_test, y_test), axis=1)
+utils = Utils(data)
+groups = utils.sorted_groups(predict_proba, n=10)
+print('lengths', [len(group) for group in groups])
+defaults_in_groups = [np.sum(group[i][0] for i in range(len(group))) for group in groups]
+print('defaults in groups:', defaults_in_groups)
+print('PDs in groups', [utils.avg_group_proba(group) for group in groups])
+
+calibration_metrics = CalibrationMetrics(utils)
+hs_statistics = calibration_metrics.hosmer_lemeshow(groups)
+brier_score = calibration_metrics.brier_score(groups)
+brier_skill_score = calibration_metrics.brier_skill_score(groups, total_test_defaults)
+
+print('H-S statistics for groups', hs_statistics)
+print('H-S statistic in total:', np.sum(hs_statistics))
+print('Brier score: ', brier_score)
+print('Brier skill score: ', brier_skill_score)
+
+from scipy.stats import chisquare
+
+p = 1 - chisquare(hs_statistics, 8)[1]
+p = "{:.50f}".format(float(p))
+print('p value for H-S:', p)
+
+traffic_lights = [calibration_metrics.normal_approximation_test(group) for group in groups]
+print('traffic lights:', traffic_lights)
--- implied_model.py
+++ implied_model.py
@@ -13,7 +13,7 @@ preprocessor.adjust_excel()
 preprocessor.convert_numbers_to_numeric()
 df.drop(columns=['ASSESSMENT_YEAR', 'GROUP_FLAG', 'TURNOVER', 'INDUSTRY'], axis=1, inplace=True)
 print(df.columns)
-y_test = df['DEFAULT_FLAG'].to_numpy() #here test data is all data
+y_test = df['DEFAULT_FLAG'].to_numpy()  # here test data is all data
 
 
 # print(df.columns)
@@ -32,7 +32,7 @@ class ImpliedModel:
     def pd(self):
         exp = np.exp(-0.1 * self.score())
         denominator = 1 + exp
-        return 1 - (1 / denominator)  # probability of reverse of non-default
+        return 1 / denominator  # probability of non-default #everything evaluates to default ie >0.5 ie 1
 
     # simple prediction, but could be done using sigmoid etc.
     def predict(self):
@@ -43,7 +43,26 @@ class ImpliedModel:
                 prediction.append(1)
             else:
                 prediction.append(0)
-        return prediction
+        # print('prediction:',prediction)
+        return prediction  # prediction is all 1
+
+    def true_positive(self, y_test):
+        true_pos = 0
+        prediction = self.predict()
+        for i in range(len(prediction)):
+            if y_test[i] == 1 & prediction[i] == y_test[i]:
+                true_pos += 1
+        # print('true positive:',true_pos)
+        return true_pos  # should be around 10% of all OK, those are truly defaulted
+
+    def true_negative(self, y_test):
+        true_neg = 0
+        prediction = self.predict()
+        for i in range(len(prediction)):
+            if prediction[i] == 0 & y_test[i] == 0:
+                true_neg += 1
+        print('true negative:', true_neg)
+        return true_neg  # should be 0
 
     # accuracy is (true_positive+true_negative)/total
     def accuracy(self, y_test):
@@ -63,49 +82,33 @@ class ImpliedModel:
         true_positive = self.true_positive(y_test)
         negative = len(y_test) - np.sum(self.predict())
         false_negative = negative - self.true_negative(y_test)
-
+        print('negative', negative)
         return np.divide(true_positive, true_positive + false_negative)
-
-    def true_positive(self, y_test):
-        true_pos = 0
-        prediction = self.predict()
-        for i in range(len(prediction)):
-            if y_test[i] == 1 & prediction[i] == y_test[i]:
-                true_pos += 1
-        return true_pos
-
-    def true_negative(self, y_test):
-        true_neg = 0
-        prediction = self.predict()
-        for i in range(len(prediction)):
-            if y_test[i] == 0 & prediction[i] == y_test[i]:
-                true_neg += 1
-        return true_neg
 
     def f1_model_score(self, y_test):
         return 2 * np.divide(self.precision(y_test) * self.recall(y_test), self.precision(y_test) + self.recall(y_test))
 
     def model_score(self, y):
         score = 0
-        prediction=self.predict()
+        prediction = self.predict()
         for i in range(len(prediction)):
             if prediction[i] == y[i]:
                 score += 1
-        return score/len(prediction)
+        return score / len(prediction)
 
 
 implied_model = ImpliedModel(df.to_numpy())
 score = implied_model.model_score(y_test)
 precision = implied_model.precision(y_test)
-recall=implied_model.recall(y_test)
+recall = implied_model.recall(y_test)
 
 predict_proba = implied_model.pd()
 prediction = implied_model.predict()
 number_of_predicted_defaults = np.sum(prediction)
 
-print('recall:',recall)
-print('precision:',precision)
-print('F1 score:',implied_model.f1_model_score(y_test))
-print('accuracy:',implied_model.accuracy(y_test))
+print('recall:', recall)
+print('precision:', precision)
+print('F1 score:', implied_model.f1_model_score(y_test))
+print('accuracy:', implied_model.accuracy(y_test))
 
 print('Score:', score)
